ab69ed8fd4b86aa5a9ebeadc7df5d7a9a8deeed3
Victor Birath <victor.birath@gmail.com>
2012-10-10 11:56:24 +0200

    Hello_World ver0.1

---
+++ Hello_World/src/Hello_World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello_World.cpp
+// Author      : Victor Birath
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
