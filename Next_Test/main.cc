e6cfab3a6a51dfe6f0cf7a7dc0fc4702332406ae
Victor Birath <victor.birath@gmail.com>
2012-11-16 11:14:27 +0100

    Test

---
+++ Next_Test/main.cc
@@ -0,0 +1,20 @@
+/*
+ * main.cc
+ *
+ *  Created on: 1 nov 2012
+ *      Author: victorbirath
+ */
+
+
+#include "communicator.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << s << endl;
+
+	return 0;
+}
