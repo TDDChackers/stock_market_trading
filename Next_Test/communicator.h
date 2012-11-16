947c18fb9272c3bcaa21ed8370afecebfc28e76d
Victor Birath <victor.birath@gmail.com>
2012-11-16 11:04:18 +0100

    test

---
+++ Next_Test/communicator.h
@@ -0,0 +1,19 @@
+/*
+ * communicator.h
+ *
+ *  Created on: 1 nov 2012
+ *      Author: victorbirath
+ */
+
+#ifndef COMMUNICATOR_H_
+#define COMMUNICATOR_H_
+
+
+class communicator {
+public:
+	communicator();
+	~communicator();
+	bool login();
+};
+
+#endif /* COMMUNICATOR_H_ */
